9d6372d97d19d8848ef1214cda4a3a94769c5a0e
Arb00 <muhammadsabri375@gmail.com>
2026-09-14 17:49:13 +0700

    Fix CSRF trusted origins

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -152,3 +152,6 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+CSRF_TRUSTED_ORIGINS = [
+    'https://muhammad-sabri51-myportofolio.pws.cs.ui.ac.id',
+]
